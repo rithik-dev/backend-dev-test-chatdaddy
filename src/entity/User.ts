bd8cb2656c762a4ed6af567d62e289775523bb43
Rithik Bhandari <rithikbhandari895@gmail.com>
2022-06-22 00:00:58 +0530

    fix: fixed compilation errors for entities

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -3,14 +3,14 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
 @Entity()
 export class User {
 	@PrimaryGeneratedColumn()
-	id: string
+		id: string
 
 	@Column()
-	name: string
+		name: string
 
 	@Column()
-	phoneNumber: string
+		phoneNumber: string
 
 	@Column()
-	age: number
+		age: number
 }
